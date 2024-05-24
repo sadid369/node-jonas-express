921c2d3bb4f6ee0954fcaffe8a723aa1431115ef
sadid369 <sadid.jones@gmail.com>
2024-05-25 02:42:55 +0600

    updated

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,12 @@ const Tour = require('../models/tourModel');
 
 
 
-
+exports.aliasTopTours = async (req, res, next) => {
+    req.query.limit = "5";
+    req.query.sort = "-ratingsAverage,price";
+    req.query.fields = 'name,price,ratingsAverage,difficulty';
+    next();
+};
 
 exports.getAllTours = async (req, res) => {
     try {     //1) Filtering
@@ -12,10 +17,6 @@ exports.getAllTours = async (req, res) => {
         excludedFields.forEach((el) => delete queryObj[el]);
         console.log(req.query, queryObj);
 
-        // const query = await Tour.find({
-        //     duration: 5,
-        //     difficulty: 'easy'
-        // }).where('duration').equals(5).where('difficulty').equals('easy');
 
 
         //2) Advance Filtering
@@ -27,15 +28,27 @@ exports.getAllTours = async (req, res) => {
         let query = Tour.find(JSON.parse(queryStr));
         if (req.query.sort) {
             const sortBy = req.query.sort.split(',').join(" ");
+            console.log(sortBy);
             query.sort(sortBy);
         } else {
-            query = query.sort('-createdAt');
+            // query = query.sort('-createdAt');
         }
         if (req.query.fields) {
             const fields = req.query.fields.split(',').join(" ");
             query = query.select(fields);
         } else {
-            query = query.select('-__v');
+            // query = query.select('-__v');
+        }
+        //pagination
+        const page = req.query.page * 1;
+        const limit = req.query.limit * 1;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if (skip >= numTours) {
+                throw new Error('This page does not exits');
+            }
         }
 
         const tours = await query;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAllTours, createTour, getTour, updateTour, deleteTour } = require('../controllers/tourController');
+const { getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours } = require('../controllers/tourController');
 const tourRouter = express.Router();
 
 
 // tourRouter.param('id', checkID)
+
+tourRouter.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
